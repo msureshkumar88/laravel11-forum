36a911a1864b008125aeb2a7bc118bc64defb901
Suresh Kumar <suresh12_dmm@yahoo.com>
2024-05-24 14:42:54 +0100

    implement post pagination"

--- resources/js/Pages/Posts/Index.vue
+++ resources/js/Pages/Posts/Index.vue
@@ -2,10 +2,12 @@
     <AppLayout>
         <container>
             <ul class="divide-y">
-                <li v-for="post in posts" class="px-2 py-4">
+                <li v-for="post in posts.data" :key="post.id" class="px-2 py-4">
                     <span class="font-bold text-lg">{{post.title}}</span>
                 </li>
             </ul>
+
+            <Pagination :meta="posts.meta" class="mt-2" />
         </container>
     </AppLayout>
 </template>
@@ -14,5 +16,6 @@
 <script setup lang="ts">
 import AppLayout from "@/Layouts/AppLayout.vue";
 import Container from "@/Components/container.vue";
+import Pagination from "@/Components/Pagination.vue";
 defineProps(['posts']);
 </script>
