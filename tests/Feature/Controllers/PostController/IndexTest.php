43d14a9ffef94b627ea247c0a78e82340c9a5f4f
Suresh Kumar <suresh12_dmm@yahoo.com>
2024-05-27 16:24:36 +0100

    add link to show post details from poat index

--- tests/Feature/Controllers/PostController/IndexTest.php
+++ tests/Feature/Controllers/PostController/IndexTest.php
@@ -11,9 +11,11 @@ it('should return correct component', function () {
             ->component('Posts/Index', true));
 });
 
-it('passes posts to the view', function (){
+it('passes posts to the view', function () {
+    $posts = \App\Models\Post::factory(3)->create();
+
+    $posts->load('user');
+
     get(route('posts.index'))
-        ->assertInertia(fn (AssertableInertia $inertia) => $inertia
-            ->has('posts')
-        );
+        ->assertHasPaginatedResource('posts', \App\Http\Resources\PostResource::collection($posts->reverse()));
 });
