3bb7fa4d2e1b9f9780b4ff5cec56fa9a7e73bc15
Suresh Kumar <suresh12_dmm@yahoo.com>
2024-05-29 15:19:56 +0100

    implement show comment per post

--- tests/Feature/Controllers/PostController/ShowTest.php
+++ tests/Feature/Controllers/PostController/ShowTest.php
@@ -13,3 +13,13 @@ it('passes a post to the view', function () {
     $post->load('user');
     get(route('posts.show', $post))->assertHasResource('post', \App\Http\Resources\PostResource::make($post));
 });
+
+
+it('passes comments to the view', function(){
+    $post = \App\Models\Post::factory()->create();
+
+    $comment = \App\Models\Comment::factory(2)->for($post)->create();
+    $comment->load('user');
+    get(route('posts.show', $post))->assertHasPaginatedResource('comment', \App\Http\Resources\CommentResource::collection($comment->reverse()));
+
+});
